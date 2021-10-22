cbb5e8b90e5979d13d9340dfc33828b1fa3f2ec3
Bejjang2 <91356113+jeseongkim@users.noreply.github.com>
2021-10-22 14:40:42 +0900

    Update 백준13460.cpp

--- 백준13460.cpp
+++ 백준13460.cpp
@@ -1,92 +1,85 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <queue>
 #include <string>
 
 using namespace std;
-#define fi first
-#define se second
 
 int n,m;
-vector<string> board0;
+vector<string> board;
+struct info
+{
+	int rx, ry, bx, by;
+};
+int dirx[4] = {-1, 1, 0, 0};
+int diry[4] = {0, 0, -1, 1};
 
-int sol(vector<string> &board1, int mov_cnt, int prev_dir);
-int go2i(vector<string> &board1, int dir);
-void findpos(vector<string> &board1, pair<int, int> &ball, char c);
+int sol(info cur, int cnt);
 
 int main(int argc, char const *argv[])
 {
-	string temp;
 	scanf("%d %d\n", &n, &m);
+	string temp;
 	for (int i = 0; i < n; ++i){
 		cin >> temp;
-		board0.push_back(temp);
+		board.push_back(temp);
 	}
 
-	int tmp, ret = 11, ret_tmp;
-	for (int i = 0; i < 4; ++i){
-		vector<string> board1(board0);
-		tmp = go2i(board1, i); // return can go or not.
-		// if (tmp){ // can move
-		// 	ret_tmp = sol(board_tmp, 1, i);
-		// 	if (ret_tmp){
-		// 		ret = min(ret_tmp, ret);
-		// 	}
-		// }
+	info cur;
+	for (int i = 0; i < n; ++i){
+		for (int j = 0; j < m; ++j){
+			if (board[i][j] == 'R'){
+				cur.rx = i; cur.ry = j; board[i][j] = '.';
+			}
+			if (board[i][j] == 'B'){
+				cur.bx = i; cur.by = j; board[i][j] = '.';
+			}
+		}
 	}
-	cout << (ret == 11 ? -1 :ret) << endl;
+
+	int ret = sol(cur, 0);
+	cout << (ret == 11? -1 : ret);
 	return 0;
 }
 
-void findpos(vector<string> &board1, pair<int, int> &ball, char c){
-	for (int i = 0; i < n; ++i){
-		for (int j = 0; j < m; ++j){
-			if (board1[i][j] == c){
-				ball.fi = i; ball.se = j;
+int sol(info cur, int cnt){
+	if (cnt >= 10){
+		return 11;
+	}
+
+	int ret = 11;
+
+	for (int dir = 0; dir < 4; ++dir){
+		info next = cur;
+		while(board[next.rx + dirx[dir]][next.ry + diry[dir]] == '.'){
+			next.rx += dirx[dir];
+			next.ry += diry[dir];
+		}
+
+		while(board[next.bx + dirx[dir]][next.by + diry[dir]] == '.'){
+			next.bx += dirx[dir];
+			next.by += diry[dir];
+		}
+		if (board[next.bx + dirx[dir]][next.by + diry[dir]] == 'O')
+			continue;
+		if (board[next.rx + dirx[dir]][next.ry + diry[dir]] == 'O'){
+			return cnt + 1;
+		}
+
+		if (next.rx == next.bx && next.ry == next.by){
+			int rdist = abs(next.rx - cur.rx + next.ry - cur.ry);
+			int bdist = abs(next.bx - cur.bx + next.by - cur.by);
+			if (rdist > bdist){
+				next.rx -= dirx[dir];
+				next.ry -= diry[dir];
+			}
+			else{
+				next.bx -= dirx[dir];
+				next.by -= diry[dir];
 			}
 		}
+		ret = min(ret, sol(next, cnt+1));
 	}
+	return ret;
 }
 
-// 0 : 
-int go2i(vector<string> &board1, int dir){
-	pair<int, int> red, blue;
-	findpos(board1, red, 'R');
-	findpos(board1, blue, 'B');
-
-	vector<string> board2(board1);
-	if (dir == 0){ // go left
-		if (red.se < blue.se){ // go red first
-			while(board1[red.fi][red.se - 1] = '.'){ // go red
-				swap(board1[red.fi][red.se - 1], board1[red.fi][red.se]);
-				red.se--;
-			}
-			while(board1[blue.fi][blue.se - 1] = '.'){ // go blue
-				swap(board1[blue.fi][blue.se - 1], board1[blue.fi][blue.se]);
-				blue.se--;
-			}
-			if (board1[red.fi][red.se] == 'O'){
-				if (/* condition */){
-					/* code */
-				}
-			}
-
-		}
-		else{
-
-		}
-	}
-	else if (dir == 1){ // go right
-		
-	}
-	else if (dir == 2){ // go right
-		
-	}
-	else{ // go right
-		
-	}
-
-	// 움직이지 않았다면 0?
-	return 1;
-}
